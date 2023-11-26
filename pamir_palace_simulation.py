6957e4497e64456649b96870a1a5622890241edf
whoisoscar <tluszczoscar@gmail.com>
2023-11-26 15:06:14 +0100

    simao changes

--- pamir_palace_simulation.py
+++ pamir_palace_simulation.py
@@ -1,4 +1,3 @@
-
 import threading
 import time
 import random
@@ -29,7 +28,16 @@ class Pool:
             print(f"Guest {guest.guest_id} is using the pool.")
             # Simulate pool usage
             time.sleep(random.uniform(0.5, 2.5))
+            if random.random() < 0.1:
+                guest.handle_emergency()
             guest.expenses += self.config.activity_costs['pool']
+
+    def handle_emergency(self):
+        print(f"Emergency for Guest {self.guest_id} at the pool!")
+        response_time = random.uniform(1, 5)
+        if response_time > 3:
+            print(f"Guest {self.guest_id} has drowned due to delayed response !")
+            self.active = False
 
 
 class Buffet:
@@ -45,13 +53,15 @@ class Buffet:
             guest.expenses += self.config.activity_costs['dine']
 
 class Reception:
-    def __init__(self, config):
+    def __init__(self, receptionist_id, config):
         self.reception_semaphore = threading.Semaphore(config.receptionists)
+        self.receptionist_id = receptionist_id
 
     def check_in(self, guest):
         with self.reception_semaphore:
-            print(f"Guest {guest.guest_id} is checking in.")
-            time.sleep(random.uniform(0.5, 1.0))
+            time.sleep(random.uniform(0.5, 1.5))
+            print(f"Receptionist {self.receptionist_id} is checking in Guest {guest.guest_id}.")
+            print(f"Guest {guest.guest_id} has checked in for {guest.nights_stay} nights.")
 
     def check_out(self, guest):
         with self.reception_semaphore:
@@ -73,28 +83,27 @@ class Gym:
 class BusExcursion:
     def __init__(self, config):
         self.config = config
-        self.bus_capacity = config.bus_capacity
         self.excursion_list = []
         self.lock = threading.Lock()
 
     def join(self, guest):
         with self.lock:
-            if len(self.excursion_list) < self.bus_capacity:
+            if len(self.excursion_list) < self.config.bus_capacity:
                 self.excursion_list.append(guest)
-                print(f"Guest {guest.guest_id} has joined the bus excursion.")
-                if len(self.excursion_list) == self.bus_capacity:
+                print(f"Guest {guest.guest_id} has shown interest for the bus excursion, there are currently {len(self.excursion_list)} guests interested, {self.config.bus_capacity - len(self.excursion_list)} missing for the bus to depart.")
+                if len(self.excursion_list) == self.config.bus_capacity:
                     self.start()
             else:
-                print(f"Bus excursion is full. Guest {guest.guest_id} cannot join now.")
+                print(f"Guest {guest.guest_id} cannot join now, bus is full.")
 
     def start(self):
         print(f"Bus excursion starting with guests {[guest.guest_id for guest in self.excursion_list]}.")
-        # Simulate excursion duration
-        time.sleep(random.uniform(1.0, 3.0))
+        time.sleep(1)  # Simulate excursion duration
         for guest in self.excursion_list:
             guest.expenses += self.config.activity_costs['bus_excursion']
         self.excursion_list.clear()
-        print("Bus excursion has returned.")
+        print(f"Bus excursion has returned with guests {[guest.guest_id for guest in self.excursion_list]}.")
+
 
 class TouristTrip:
     def __init__(self, config):
@@ -119,22 +128,33 @@ class Guest(threading.Thread):
         self.active = True
         
     def run(self):
-        self.hotel.reception.check_in(self)
-        while self.nights_stay > 0:
-            activity = self.choose_activity()
-            activity(self)
-            time.sleep(3)  # Time between activities
-            self.nights_stay -= 1
-        self.hotel.reception.check_out(self)
+        try:
+            self.hotel.reception.check_in(self)
+            while self.nights_stay > 0:
+                activity = self.choose_activity()
+                activity(self)
+                self.nights_stay -= 1
+                time.sleep(3)  # Time between activities
+            self.hotel.reception.check_out(self)
+        except Exception as e:
+            print(f"Error in Guest {self.guest_id}: {e}")
 
     def choose_activity(self):
-        activities = [self.hotel.pool.use, self.hotel.buffet.dine, self.hotel.gym.use, 
-                      self.hotel.bus_excursion.join, self.hotel.tourist_trip.join]
-        probabilities = [self.config.activity_probabilities[activity.__name__.split('.')[1]] for activity in activities]
-        return random.choices(activities, probabilities, k=1)[0]
 
-    def handle_emergency(self):
-        print(f"Emergency for Guest {self.guest_id}!")
+        activities = [self.hotel.pool.use, self.hotel.buffet.dine, self.hotel.gym.use, 
+                        self.hotel.bus_excursion.join, self.hotel.tourist_trip.join]
+        
+        probabilities = [self.config.activity_probabilities['pool'], self.config.activity_probabilities['dine'],
+                            self.config.activity_probabilities['gym'], self.config.activity_probabilities['bus_excursion'],
+                            self.config.activity_probabilities['tourist_trip']]
+
+        chosen_activity = random.choices(activities, probabilities, k=1)[0]
+        return chosen_activity
+        
+    def check_out(self):
+        self.active = False
+        self.hotel.reception.check_out(self)
+        print(f"Guest {self.guest_id} is checking out. Total expenses: {self.expenses}$")
 
 class Hotel:
     def __init__(self, num_guests, config):
@@ -151,8 +171,20 @@ class Hotel:
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.guests)) as executor:
             for guest in self.guests:
                 executor.submit(guest.run)
+        self.simulate_days_passing()
 
+    def simulate_days_passing(self):
+        while any(guest.active for guest in self.guests):
+            time.sleep(20)  # Simulate a day passing
+            for guest in self.guests:
+                guest.nights_stay -= 1
+                if guest.nights_stay <= 0 and guest.active:
+                    guest.check_out()
 # Start the simulation
+
+config = HotelConfig()
+
+
 if __name__ == "__main__":
     config = HotelConfig()
     hotel = Hotel(50, config)  # Number of guests
